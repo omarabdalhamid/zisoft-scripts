706b902efcef25a7c9902dd9a519bc69836fe5e2
omarabdalhamid <33742475+omarabdalhamid@users.noreply.github.com>
2019-12-01 16:00:12 +0200

    Update awareness-demo.sh

--- awareness-demo.sh
+++ awareness-demo.sh
@@ -135,6 +135,8 @@ echo "\n#############################################"
 
 sudo zisoft deploy --prod
 
+sleep 5m
+
 container_web_id="$(sudo docker ps | grep web | awk '{print $1}')"
 
 sudo docker exec -it $container_web_id bash -c "php artisan db:seed --class=init"
@@ -148,6 +150,16 @@ sudo docker exec -it $container_web_id bash -c "php artisan zinad:lesson passwor
 sudo docker exec -it $container_web_id bash -c "php artisan zinad:lesson social 1 720 prod"
 
 sudo docker exec -it $container_web_id bash -c "php artisan zinad:lesson wifi 1 720 prod"
+
+
+sudo docker exec -it $container_web_id bash -c "php artisan db:seed --class=DropRecreateDB"
+
+
+sudo docker exec -it $container_web_id bash -c "php artisan migrate"
+
+
+sudo docker exec -it $container_web_id bash -c "php artisan db:seed --class=init"
+
 
 sudo docker exec -it $container_web_id bash -c "php artisan zisoft:demo 100 5 30"
 

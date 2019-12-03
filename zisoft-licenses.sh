d2a631d638015021f3d5de69164110912307a455
omarabdalhamid <33742475+omarabdalhamid@users.noreply.github.com>
2019-12-03 08:53:58 +0200

    Create zisoft-licenses.sh

---
+++ zisoft-licenses.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+################################################################################
+# Script for installing ZiSoft on Ubuntu 14.04, 15.04, 16.04 and 18.04 (could be used for other version too)
+# Author: OmarAbdalhamid Omar
+#-------------------------------------------------------------------------------
+# This script will install ZiSoft Awareness 3 on your Ubuntu 18.04 server. I
+#-------------------------------------------------------------------------------
+# Make a new file:
+# sudo nano zisoft-install.sh
+# Place this content in it and then make the file executable:
+# sudo chmod +x zisoft-licenses.sh
+# Execute the script to install zisoft:
+# ./zisoft-licenses.sh
+################################################################################
+
+echo "\n#############################################"
+
+echo  "\n--- Generate ZiSoft Licenses --"
+
+echo "\n#############################################"
+
+read -p "Enter ZiSoft Awareness  Client Name :   "  client_name
+
+read -p "Enter  Number of Users :   "  client_users
+
+read -p "Enter Number of Phishing_Users :   "  phishing_users
+
+
+container_web_id="$(sudo docker ps | grep zisoft/awareness/web | awk '{print $1}')"
+
+sudo docker exec -it $container_web_id bash -c "php artisan zisoft:license_create $client_name 2019-06-12 $client_users 2021-06-11 $phishing_users"
+
+
+echo "\n#############################################"
+
+echo  "\n---  ZiSoft Licenses Created Successfully --"
+ 
+echo "\n#############################################"
+
+echo "\n Licenses Import instructions"
+
+echo "\n 1 - Copy Licenses Activation Key"
+echo "\n 2 - Login   with an admin account"
+echo "\n 3 - Go to Administrator -> Settings -> Licenses"
+echo "\n 4 - Click + Import License"
+echo '\n 5 - paste the activation key which looks like {"users": X, "client": XXXX, "date": XXXX}XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+echo "\n 6 - Click Save"
+
+
+
+
+
+
+

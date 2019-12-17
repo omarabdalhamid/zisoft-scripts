4dee927b411359b8adc0f6e33c9a35068fcbd1f7
omarabdalhamid <33742475+omarabdalhamid@users.noreply.github.com>
2019-12-17 08:45:45 +0200

    Create zisoft-2-7-1-pull.sh

---
+++ zisoft-2-7-1-pull.sh
@@ -0,0 +1,7 @@
+docker pull registry.gitlab.com/zisoft/image/awareness/worker:2.7.1-p
+docker pull registry.gitlab.com/zisoft/image/awareness/ui:2.7.1-p
+docker pull registry.gitlab.com/zisoft/image/awareness/web:2.7.1-p
+docker pull registry.gitlab.com/zisoft/image/awareness/cron:2.7.1-p
+docker pull registry.gitlab.com/zisoft/image/awareness/db:2.7.1-p
+docker pull registry.gitlab.com/zisoft/image/awareness/proxy:2.7.1-p
+docker pull registry.gitlab.com/zisoft/image/awareness/meta:2.7.1-p

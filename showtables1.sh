c8fd92909028aa2b5e07d3fc026fc02a6f5c8a6c
omarabdalhamid <33742475+omarabdalhamid@users.noreply.github.com>
2020-04-08 22:34:23 +0200

    Create showtables1.sh

---
+++ showtables1.sh
@@ -0,0 +1,437 @@
+
+#!/bin/bash/
+
+mysql -D zisoft -u root -pPassword1  -e  "UPDATE email_servers SET username = 'awareness30@zinad.net' WHERE id = 2;"
+
+email_now=$(mysql -D zisoft -u root -pPassword1  -e "SELECT username FROM email_servers WHERE id=2";)
+
+
+show_email_now="Email changed   at   $(date)   to   $email_now"
+
+echo $show_email_now >> email_change.log
+
+################################  Wait 10 Minute ##################################################################
+
+
+sleep 10m
+
+mysql -D zisoft -u root -pPassword1  -e  "UPDATE email_servers SET username = 'awareness29@zinad.net' WHERE id = 2;"
+
+email_now=$(mysql -D zisoft -u root -pPassword1  -e "SELECT username FROM email_servers WHERE id=2";)
+
+
+show_email_now="Email changed   at   $(date)   to   $email_now"
+
+echo $show_email_now >> email_change.log
+
+################################  Wait 10 Minute ##################################################################
+
+
+sleep 10m
+
+mysql -D zisoft -u root -pPassword1  -e  "UPDATE email_servers SET username = 'awareness28@zinad.net' WHERE id = 2;"
+
+email_now=$(mysql -D zisoft -u root -pPassword1  -e "SELECT username FROM email_servers WHERE id=2";)
+
+
+show_email_now="Email changed   at   $(date)   to   $email_now"
+
+echo $show_email_now >> email_change.log
+
+################################  Wait 10 Minute ##################################################################
+
+
+sleep 10m
+
+mysql -D zisoft -u root -pPassword1  -e  "UPDATE email_servers SET username = 'awareness27@zinad.net' WHERE id = 2;"
+
+email_now=$(mysql -D zisoft -u root -pPassword1  -e "SELECT username FROM email_servers WHERE id=2";)
+
+
+show_email_now="Email changed   at   $(date)   to   $email_now"
+
+echo $show_email_now >> email_change.log
+
+################################  Wait 10 Minute ##################################################################
+
+
+sleep 10m
+
+mysql -D zisoft -u root -pPassword1  -e  "UPDATE email_servers SET username = 'awareness26@zinad.net' WHERE id = 2;"
+
+email_now=$(mysql -D zisoft -u root -pPassword1  -e "SELECT username FROM email_servers WHERE id=2";)
+
+
+show_email_now="Email changed   at   $(date)   to   $email_now"
+
+echo $show_email_now >> email_change.log
+
+################################  Wait 10 Minute ##################################################################
+
+
+sleep 10m
+
+mysql -D zisoft -u root -pPassword1  -e  "UPDATE email_servers SET username = 'awareness25@zinad.net' WHERE id = 2;"
+
+email_now=$(mysql -D zisoft -u root -pPassword1  -e "SELECT username FROM email_servers WHERE id=2";)
+
+
+show_email_now="Email changed   at   $(date)   to   $email_now"
+
+echo $show_email_now >> email_change.log
+
+################################  Wait 10 Minute ##################################################################
+
+
+sleep 10m
+
+mysql -D zisoft -u root -pPassword1  -e  "UPDATE email_servers SET username = 'awareness24@zinad.net' WHERE id = 2;"
+
+email_now=$(mysql -D zisoft -u root -pPassword1  -e "SELECT username FROM email_servers WHERE id=2";)
+
+
+show_email_now="Email changed   at   $(date)   to   $email_now"
+
+echo $show_email_now >> email_change.log
+
+################################  Wait 10 Minute ##################################################################
+
+
+sleep 10m
+
+mysql -D zisoft -u root -pPassword1  -e  "UPDATE email_servers SET username = 'awareness23@zinad.net' WHERE id = 2;"
+
+email_now=$(mysql -D zisoft -u root -pPassword1  -e "SELECT username FROM email_servers WHERE id=2";)
+
+
+show_email_now="Email changed   at   $(date)   to   $email_now"
+
+echo $show_email_now >> email_change.log
+
+################################  Wait 10 Minute ##################################################################
+
+
+sleep 10m
+
+mysql -D zisoft -u root -pPassword1  -e  "UPDATE email_servers SET username = 'awareness22@zinad.net' WHERE id = 2;"
+
+email_now=$(mysql -D zisoft -u root -pPassword1  -e "SELECT username FROM email_servers WHERE id=2";)
+
+
+show_email_now="Email changed   at   $(date)   to   $email_now"
+
+echo $show_email_now >> email_change.log
+
+################################  Wait 10 Minute ##################################################################
+
+
+sleep 10m
+
+mysql -D zisoft -u root -pPassword1  -e  "UPDATE email_servers SET username = 'awareness21@zinad.net' WHERE id = 2;"
+
+email_now=$(mysql -D zisoft -u root -pPassword1  -e "SELECT username FROM email_servers WHERE id=2";)
+
+
+show_email_now="Email changed   at   $(date)   to   $email_now"
+
+echo $show_email_now >> email_change.log
+
+################################  Wait 10 Minute ##################################################################
+
+
+sleep 10m
+
+mysql -D zisoft -u root -pPassword1  -e  "UPDATE email_servers SET username = 'awareness20@zinad.net' WHERE id = 2;"
+
+email_now=$(mysql -D zisoft -u root -pPassword1  -e "SELECT username FROM email_servers WHERE id=2";)
+
+
+show_email_now="Email changed   at   $(date)   to   $email_now"
+
+echo $show_email_now >> email_change.log
+
+################################  Wait 10 Minute ##################################################################
+
+
+sleep 10m
+
+mysql -D zisoft -u root -pPassword1  -e  "UPDATE email_servers SET username = 'awareness19@zinad.net' WHERE id = 2;"
+
+email_now=$(mysql -D zisoft -u root -pPassword1  -e "SELECT username FROM email_servers WHERE id=2";)
+
+
+show_email_now="Email changed   at   $(date)   to   $email_now"
+
+echo $show_email_now >> email_change.log
+
+################################  Wait 10 Minute ##################################################################
+
+
+sleep 10m
+
+mysql -D zisoft -u root -pPassword1  -e  "UPDATE email_servers SET username = 'awareness18@zinad.net' WHERE id = 2;"
+
+email_now=$(mysql -D zisoft -u root -pPassword1  -e "SELECT username FROM email_servers WHERE id=2";)
+
+
+show_email_now="Email changed   at   $(date)   to   $email_now"
+
+echo $show_email_now >> email_change.log
+
+################################  Wait 10 Minute ##################################################################
+
+
+sleep 10m
+
+mysql -D zisoft -u root -pPassword1  -e  "UPDATE email_servers SET username = 'awareness17@zinad.net' WHERE id = 2;"
+
+email_now=$(mysql -D zisoft -u root -pPassword1  -e "SELECT username FROM email_servers WHERE id=2";)
+
+
+show_email_now="Email changed   at   $(date)   to   $email_now"
+
+echo $show_email_now >> email_change.log
+
+################################  Wait 10 Minute ##################################################################
+
+
+sleep 10m
+
+mysql -D zisoft -u root -pPassword1  -e  "UPDATE email_servers SET username = 'awareness16@zinad.net' WHERE id = 2;"
+
+email_now=$(mysql -D zisoft -u root -pPassword1  -e "SELECT username FROM email_servers WHERE id=2";)
+
+
+show_email_now="Email changed   at   $(date)   to   $email_now"
+
+echo $show_email_now >> email_change.log
+
+################################  Wait 10 Minute ##################################################################
+
+
+sleep 10m
+
+mysql -D zisoft -u root -pPassword1  -e  "UPDATE email_servers SET username = 'awareness15@zinad.net' WHERE id = 2;"
+
+email_now=$(mysql -D zisoft -u root -pPassword1  -e "SELECT username FROM email_servers WHERE id=2";)
+
+
+show_email_now="Email changed   at   $(date)   to   $email_now"
+
+echo $show_email_now >> email_change.log
+
+################################  Wait 10 Minute ##################################################################
+
+
+sleep 10m
+
+mysql -D zisoft -u root -pPassword1  -e  "UPDATE email_servers SET username = 'awareness14@zinad.net' WHERE id = 2;"
+
+email_now=$(mysql -D zisoft -u root -pPassword1  -e "SELECT username FROM email_servers WHERE id=2";)
+
+
+show_email_now="Email changed   at   $(date)   to   $email_now"
+
+echo $show_email_now >> email_change.log
+
+################################  Wait 10 Minute ##################################################################
+
+
+sleep 10m
+
+mysql -D zisoft -u root -pPassword1  -e  "UPDATE email_servers SET username = 'awareness13@zinad.net' WHERE id = 2;"
+
+email_now=$(mysql -D zisoft -u root -pPassword1  -e "SELECT username FROM email_servers WHERE id=2";)
+
+
+show_email_now="Email changed   at   $(date)   to   $email_now"
+
+echo $show_email_now >> email_change.log
+
+################################  Wait 10 Minute ##################################################################
+
+
+sleep 10m
+
+mysql -D zisoft -u root -pPassword1  -e  "UPDATE email_servers SET username = 'awareness12@zinad.net' WHERE id = 2;"
+
+email_now=$(mysql -D zisoft -u root -pPassword1  -e "SELECT username FROM email_servers WHERE id=2";)
+
+
+show_email_now="Email changed   at   $(date)   to   $email_now"
+
+echo $show_email_now >> email_change.log
+
+################################  Wait 10 Minute ##################################################################
+
+
+sleep 10m
+
+mysql -D zisoft -u root -pPassword1  -e  "UPDATE email_servers SET username = 'awareness11@zinad.net' WHERE id = 2;"
+
+email_now=$(mysql -D zisoft -u root -pPassword1  -e "SELECT username FROM email_servers WHERE id=2";)
+
+
+show_email_now="Email changed   at   $(date)   to   $email_now"
+
+echo $show_email_now >> email_change.log
+
+################################  Wait 10 Minute ##################################################################
+
+
+sleep 10m
+
+mysql -D zisoft -u root -pPassword1  -e  "UPDATE email_servers SET username = 'awareness10@zinad.net' WHERE id = 2;"
+
+email_now=$(mysql -D zisoft -u root -pPassword1  -e "SELECT username FROM email_servers WHERE id=2";)
+
+
+show_email_now="Email changed   at   $(date)   to   $email_now"
+
+echo $show_email_now >> email_change.log
+
+################################  Wait 10 Minute ##################################################################
+
+
+sleep 10m
+
+mysql -D zisoft -u root -pPassword1  -e  "UPDATE email_servers SET username = 'awareness9@zinad.net' WHERE id = 2;"
+
+email_now=$(mysql -D zisoft -u root -pPassword1  -e "SELECT username FROM email_servers WHERE id=2";)
+
+
+show_email_now="Email changed   at   $(date)   to   $email_now"
+
+echo $show_email_now >> email_change.log
+
+################################  Wait 10 Minute ##################################################################
+
+
+sleep 10m
+
+mysql -D zisoft -u root -pPassword1  -e  "UPDATE email_servers SET username = 'awareness8@zinad.net' WHERE id = 2;"
+
+email_now=$(mysql -D zisoft -u root -pPassword1  -e "SELECT username FROM email_servers WHERE id=2";)
+
+
+show_email_now="Email changed   at   $(date)   to   $email_now"
+
+echo $show_email_now >> email_change.log
+
+################################  Wait 10 Minute ##################################################################
+
+
+sleep 10m
+
+mysql -D zisoft -u root -pPassword1  -e  "UPDATE email_servers SET username = 'awareness7@zinad.net' WHERE id = 2;"
+
+email_now=$(mysql -D zisoft -u root -pPassword1  -e "SELECT username FROM email_servers WHERE id=2";)
+
+
+show_email_now="Email changed   at   $(date)   to   $email_now"
+
+echo $show_email_now >> email_change.log
+
+################################  Wait 10 Minute ##################################################################
+
+
+sleep 10m
+
+mysql -D zisoft -u root -pPassword1  -e  "UPDATE email_servers SET username = 'awareness6@zinad.net' WHERE id = 2;"
+
+email_now=$(mysql -D zisoft -u root -pPassword1  -e "SELECT username FROM email_servers WHERE id=2";)
+
+
+show_email_now="Email changed   at   $(date)   to   $email_now"
+
+echo $show_email_now >> email_change.log
+
+################################  Wait 10 Minute ##################################################################
+
+
+sleep 10m
+
+mysql -D zisoft -u root -pPassword1  -e  "UPDATE email_servers SET username = 'awareness5@zinad.net' WHERE id = 2;"
+
+email_now=$(mysql -D zisoft -u root -pPassword1  -e "SELECT username FROM email_servers WHERE id=2";)
+
+
+show_email_now="Email changed   at   $(date)   to   $email_now"
+
+echo $show_email_now >> email_change.log
+
+################################  Wait 10 Minute ##################################################################
+
+
+sleep 10m
+
+mysql -D zisoft -u root -pPassword1  -e  "UPDATE email_servers SET username = 'awareness4@zinad.net' WHERE id = 2;"
+
+email_now=$(mysql -D zisoft -u root -pPassword1  -e "SELECT username FROM email_servers WHERE id=2";)
+
+
+show_email_now="Email changed   at   $(date)   to   $email_now"
+
+echo $show_email_now >> email_change.log
+
+################################  Wait 10 Minute ##################################################################
+
+
+sleep 10m
+
+mysql -D zisoft -u root -pPassword1  -e  "UPDATE email_servers SET username = 'awareness3@zinad.net' WHERE id = 2;"
+
+email_now=$(mysql -D zisoft -u root -pPassword1  -e "SELECT username FROM email_servers WHERE id=2";)
+
+
+show_email_now="Email changed   at   $(date)   to   $email_now"
+
+echo $show_email_now >> email_change.log
+
+################################  Wait 10 Minute ##################################################################
+
+
+sleep 10m
+
+mysql -D zisoft -u root -pPassword1  -e  "UPDATE email_servers SET username = 'awareness2@zinad.net' WHERE id = 2;"
+
+email_now=$(mysql -D zisoft -u root -pPassword1  -e "SELECT username FROM email_servers WHERE id=2";)
+
+
+show_email_now="Email changed   at   $(date)   to   $email_now"
+
+echo $show_email_now >> email_change.log
+
+################################  Wait 10 Minute ##################################################################
+
+
+sleep 10m
+
+mysql -D zisoft -u root -pPassword1  -e  "UPDATE email_servers SET username = 'awareness1@zinad.net' WHERE id = 2;"
+
+email_now=$(mysql -D zisoft -u root -pPassword1  -e "SELECT username FROM email_servers WHERE id=2";)
+
+
+show_email_now="Email changed   at   $(date)   to   $email_now"
+
+echo $show_email_now >> email_change.log
+
+################################  Wait 10 Minute ##################################################################
+
+
+
+sleep 10m
+
+mysql -D zisoft -u root -pPassword1  -e  "UPDATE email_servers SET username = 'awareness@zinad.net' WHERE id = 2;"
+
+email_now=$(mysql -D zisoft -u root -pPassword1  -e "SELECT username FROM email_servers WHERE id=2";)
+
+
+show_email_now="From First loop  Email changed   at   $(date)   to   $email_now"
+
+echo $show_email_now >> email_change.log
+
+################################  Wait 10 Minute ##################################################################
+
+
+nohup ./showtables1.sh &

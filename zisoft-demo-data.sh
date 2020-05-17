3a2046d6e574130994d8961daa44c2391ea9f826
omarabdalhamid <33742475+omarabdalhamid@users.noreply.github.com>
2020-05-17 17:04:14 +0200

    Update and rename load-demo-data.sh to zisoft-demo-data.sh

--- zisoft-demo-data.sh
+++ zisoft-demo-data.sh
@@ -4,14 +4,6 @@ container_ui_id="$(sudo docker ps | grep zisoft/awareness/ui | awk '{print $1}')
 
 
 
-sudo docker exec -it $container_web_id bash -c 'sed -i "/zinad:lessons/a '\''campaign1'\'' => 1"  app/Console/Commands/Demo.php'
-
-sudo docker exec -it $container_web_id bash -c 'sed -i "/zinad:lessons/a '\''mode'\'' => '\''none'\'',"  app/Console/Commands/Demo.php'
-
-sudo docker exec -it $container_web_id bash -c 'sed -i "/zinad:lessons/a '\''resolution'\'' => '\''720'\'',"  app/Console/Commands/Demo.php'
-
-sudo docker exec -it $container_web_id bash -c 'sed -i "/zinad:lessons/a '\''version'\'' => 1,"  app/Console/Commands/Demo.php'
-
 
 sudo docker exec -it $container_web_id bash -c "php artisan db:seed --class=init"
 
@@ -30,13 +22,7 @@ sudo docker exec -it $container_web_id bash -c "php artisan zinad:lesson aml 1 7
 sudo docker exec -it $container_web_id bash -c "php artisan zinad:lessons 720 1 720 prod"
 
 
-sudo docker exec -it $container_web_id bash -c "php artisan db:seed --class=DropRecreateDB"
-
-
 sudo docker exec -it $container_web_id bash -c "php artisan migrate"
-
-
-sudo docker exec -it $container_web_id bash -c "php artisan db:seed --class=init"
 
 
 sudo docker exec -it $container_web_id bash -c "php artisan zisoft:demo 100 5 30"

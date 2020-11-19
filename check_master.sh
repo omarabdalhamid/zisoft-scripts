f7c95002f3d49dfd24d87a1abc71698e7d32631d
omarabdalhamid <33742475+omarabdalhamid@users.noreply.github.com>
2020-11-19 12:52:24 +0200

    Update check_master.sh

--- check_master.sh
+++ check_master.sh
@@ -3,6 +3,8 @@
 git rev-list HEAD > check_master.txt 
 remote_master_sha=$(git ls-remote https://gitlab+deploy-token-281128:k8sN7KjmBXXjUKx9CKst@gitlab.com/zisoft/awareness3.1.git HEAD | awk '{ print $1}')
 
+echo "Commit ID of Master: $remote_master_sha"
+cat check_master.txt
 if grep -Fxq "$remote_master_sha"  ./check_master.txt
 then
     echo "Master Merged Successfully" 

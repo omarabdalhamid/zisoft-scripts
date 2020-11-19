b073d4ac7c04833a1a08f7ca335255c94a301fa2
omarabdalhamid <33742475+omarabdalhamid@users.noreply.github.com>
2020-11-19 10:17:31 +0200

    Update Check_master.sh

--- Check_master.sh
+++ Check_master.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
-rm -rf /home/gitlab-runner/pipeline/branch/$CI_COMMIT_SHORT_SHA
-mkdir /home/gitlab-runner/pipeline/branch/$CI_COMMIT_SHORT_SHA
-cd  /home/gitlab-runner/pipeline/branch/$CI_COMMIT_SHORT_SHA
-git clone https://gitlab+deploy-token-281128:k8sN7KjmBXXjUKx9CKst@gitlab.com/itsys/demo-project.git 
-cd $CI_PROJECT_NAME
-git checkout $CI_COMMIT_REF_NAME
-git rev-list HEAD > check_master.txt 
-remote_master_sha=$(git ls-remote https://gitlab+deploy-token-281128:k8sN7KjmBXXjUKx9CKst@gitlab.com/itsys/demo-project.git HEAD | awk '{ print $1}')
 
+git rev-list HEAD > check_master.txt 
+remote_master_sha=$(git ls-remote https://gitlab+deploy-token-281128:k8sN7KjmBXXjUKx9CKst@gitlab.com/zisoft/awareness3.1.git HEAD | awk '{ print $1}')
 
 if grep -Fxq "$remote_master_sha"  ./check_master.txt
 then
-    echo "Master Merged Successfully"
+    echo "Master Merged Successfully" 
     exit 0
 else
-    echo "Please merger master before push to your branch"
+    echo "Please merge master before push to your branch" 
     exit 1 
 fi
